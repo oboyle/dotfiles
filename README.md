ff4009f3ae599cf759a336361ef399d514fbf11a
oboyle <oboyle.todd@gmail.com>
2019-02-05 12:52:51 -0500

    Move to Python 3 and fix readme.

--- README.md
+++ README.md
@@ -4,19 +4,15 @@ When setting up a new mac:
 
 * install xcode via app store
 * install homebrew - see website
-* install caskroom - `brew install caskroom/cask/brew-cask`
-* install apps via cask
+
 
 ```
 brew cask install atom \
   firefox \
-  flux \
   google-chrome \
-  rdio \
   spectacle \
   virtualbox \
-  vagrant \
-  lastpass
+  vagrant
 ```
 
 * install deps via brew
@@ -25,16 +21,23 @@ brew cask install atom \
 brew install bash \
   byobu \
   git \
-  irssi \
   coreutils \
   findutils \
   node \
-  python
+  python \
+  zsh
 ```
+
+* install virtualenvwrapper
+
+`python -m pip install virtualenvwrapper`
 
 * install oh-my-zsh
 
 ```
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ```
 
+* `python bootstrap.py`
+
+* pick your theme from https://github.com/robbyrussell/oh-my-zsh/wiki/Themes and add it to your .zshrc.
--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/local/bin/python3
 
 # Basic Imports
 import os
 import sys
+from os.path import expanduser
 
 # Global - add files you want to ignore in the current directory
 IGNORED_FILES = ["bootstrap.py", "README.md", "LICENSE", ".DS_Store"]
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     home = os.getenv("HOME")
 
     if not currentDirectory.endswith("dotfiles"):
-        print "Are you running this outside of the dotfiles directory?"
+        print("Are you running this outside of the dotfiles directory?")
         sys.exit(1)
 
     else:
@@ -25,17 +26,20 @@ if __name__ == "__main__":
 
         for f in dotfiles:
             if f.endswith("~") or os.path.isdir(f) or (f in IGNORED_FILES):
-                print "Ignoring temp file or directory: %s" % f
+                print("Ignoring temp file or directory: %s" % f)
                 continue
             
             elif f.startswith("."):
                 original = currentDirectory + "/" + f
                 homeFile = home + "/" + f
-                print "Linking: %s --> %s" % (original, homeFile)
-                os.system("ln -si %s %s" % (original, homeFile))
+                print("Linking: %s --> %s" % (original, homeFile))
+                os.system("ln -fsi %s %s" % (original, homeFile))
 
             else:
-                print "We encountered some weird file and are ignoring it (" + f + ")"
+                print("We encountered some weird file and are ignoring it %s" % f)
 
-    print "Done linking..."
+        workon_home = os.getenv("WORKON_HOME", expanduser("~") + "/code/environments") 
+        print("Making virtualenv directory: %s" % workon_home)
+        os.makedirs(workon_home, exist_ok=True)
+    print("Done linking...")
 
